6ff470aaebd03e90491c7d0ba7217e17f56cf6ae
Melissa Shields <mshieldsny@gmail.com>
2013-03-29 18:04:19 -0400

    This is my first completed homework assignment

---
+++ hw1/HW_01.rb
@@ -0,0 +1,56 @@
+###############################################################################
+#
+# Introduction to Ruby on Rails
+#
+# HW 01
+#
+# Purpose:
+#
+# Read the taks below and complete the exercises in this file. We will start
+# to write the beginnings of our "Secret Number Game" using what we've
+# learned in Ruby Lesson 01.
+#
+###############################################################################
+#
+# 1. Read an Interview with the Creator of Ruby
+# 	 http://linuxdevcenter.com/pub/a/linux/2001/11/29/ruby.html
+#
+# 2. Read this Introduction to Programming
+# 	 http://en.wikiversity.org/wiki/Introduction_to_Programming/About_Programming
+#
+# 3. In this file under "Student Solution," print the welcome text of your Secret 
+#    Number Game
+#
+#	(i.e.) "Welcome to the Secret Number Game!"
+#
+# 4. Above your welcome message, write a comment to other coders introducing yourself.
+#	 .
+#
+#
+# 5. Create two new variables, one for your last name, one for your first name
+#    and enter your first and last name as strings.
+#
+#    first_name = ""
+#    last_name = ""
+#
+# 6. Print to the screen that your game was created by you by concating the
+#    first and last name variables.
+#
+#    (i.e.) "Created by " + first_name + " " + last_name
+#
+# 7. Feel free to add more lines of text or add comments to remind you of what
+#    you've learned.
+#
+###############################################################################
+#
+# Student Solution
+#
+###############################################################################
+#Hi, I'm Melissa Shields and this is a homework assignment for General Assembly's "Front End and Ruby-on-Rails" course.
+#
+puts "Welcome to the Secret Number Game!"
+
+first_name = "Melissa"
+last_name = "Shields"
+
+puts "This game was created by " + first_name + " " + last_name
